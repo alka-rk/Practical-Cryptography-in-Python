9d5cfe712cefa4c5a1f907da05a36f6152e7e0b6
Alka Rani Kerketta <alkaranikerketta102@gmail.com>
2026-06-03 17:19:52 +0530

    MD5 of a file

--- Chapter_2/Ex_2.4.py
+++ Chapter_2/Ex_2.4.py
@@ -1,1 +1,15 @@
+# Exercise 2.4
+# MD5 OF A FILE
 
+import hashlib
+
+file_name = input("Enter file name: ")
+
+try:
+    with open(file_name, 'rb') as file:
+        content = file.read()
+        md5_hasher = hashlib.md5(content).hexdigest()
+        print(f"The MD5 for the given file is\n{md5_hasher}")
+
+except FileNotFoundError:
+    print("File not found!")

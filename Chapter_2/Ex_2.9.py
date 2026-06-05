263649e32a71cb91aa51c24693075b42dbc69c3a
Alka Rani Kerketta <alkaranikerketta102@gmail.com>
2026-06-05 16:47:48 +0530

    Proof of Work

--- Chapter_2/Ex_2.9.py
+++ Chapter_2/Ex_2.9.py
@@ -1,1 +1,21 @@
+# Exercise 2.9
+# PROOF OF WORK
 
+import hashlib
+
+target = 2**254
+
+nonce = 0
+message = 'Cryptography'
+
+while True:
+
+    test_message = message + str(nonce)
+    hash = hashlib.sha256(test_message.encode()).hexdigest()
+    int_hash = int(hash, 16)
+
+    if int_hash < target:
+        print(f"Target found with nonce {nonce} and message: {test_message}")
+        break
+
+    nonce +=1

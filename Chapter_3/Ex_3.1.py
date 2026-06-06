1f86b0a1f0b2b0d404dba34af777fda47d4caf01
Alka Rani Kerketta <alkaranikerketta102@gmail.com>
2026-06-06 23:01:32 +0530

    A secret message

--- Chapter_3/Ex_3.1.py
+++ Chapter_3/Ex_3.1.py
@@ -1,1 +1,18 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes  
+from cryptography.hazmat.backends import default_backend
+import os
 
+key = os.urandom(16)
+aesCipher = Cipher(algorithms.AES(key),
+                    modes.ECB(),
+                    backend=default_backend())
+aesEncryptor = aesCipher.encryptor()
+aesDecryptor = aesCipher.decryptor()
+
+plaintext = 'a secret message'
+
+ciphertext = aesEncryptor.update(plaintext.encode())
+print(f"Ciphertext: {ciphertext}")
+
+decoded = aesDecryptor.update(ciphertext)
+print(f"Decoded text: {decoded.decode()}")
